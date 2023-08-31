de13394c6ca095929d7ef5aa844650d665131fd8
Tina Zeng <121203814+txinjiez@users.noreply.github.com>
2023-08-31 12:27:31 -0400

    Update 01_🚀_Main.py

--- 01_🚀_Main.py
+++ 01_🚀_Main.py
@@ -22,7 +22,7 @@ st.set_page_config(
      page_icon="📍",
      initial_sidebar_state="expanded",
      menu_items={
-         'About': "Show where you want community imporvemnts!"
+         'About': "participatory community mapping tool!"
      }
 )
 

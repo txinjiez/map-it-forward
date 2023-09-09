3f703bc7c1ac5fe0f1da084d11a66cd6d51c22aa
Tina Zeng <121203814+txinjiez@users.noreply.github.com>
2023-09-09 17:56:40 -0400

    Update 03_📜_About.py

--- pages/03_📜_About.py
+++ pages/03_📜_About.py
@@ -3,3 +3,15 @@ import streamlit as st
 st.header("About")
 
 st.markdown("Map It Forward is a participatory community mapping tool and platform.")
+
+
+multi = '''
+WELCOME to Map It Forward! Wanna improve the community?
+
+'''
+
+st.subheader("How do I contribute?", anchor=None, *, help=None, divider=False)
+
+st.markdown("We LOVE contributors 🤩. If you have any ideas for questions or want to help with the code go here: ")
+
+st.markdown("Daniel Halpern, Ryan Zhang, Tina Zeng")

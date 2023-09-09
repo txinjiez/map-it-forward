ef6f3cea4ca762341a153e6fd9cfe0a2bb7940d6
Tina Zeng <121203814+txinjiez@users.noreply.github.com>
2023-09-09 17:57:31 -0400

    Update 03_📜_About.py

--- pages/03_📜_About.py
+++ pages/03_📜_About.py
@@ -5,11 +5,6 @@ st.header("About")
 st.markdown("Map It Forward is a participatory community mapping tool and platform.")
 
 
-multi = '''
-WELCOME to Map It Forward! Wanna improve the community?
-
-'''
-
 st.subheader("How do I contribute?", anchor=None, *, help=None, divider=False)
 
 st.markdown("We LOVE contributors 🤩. If you have any ideas for questions or want to help with the code go here: ")

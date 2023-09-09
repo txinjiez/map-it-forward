a437b10696fd11656c121552ac10bfb7bd78b84f
Tina Zeng <121203814+txinjiez@users.noreply.github.com>
2023-09-09 17:30:32 -0400

    Update 03_📜_About.py

--- pages/03_📜_About.py
+++ pages/03_📜_About.py
@@ -12,3 +12,9 @@ st.set_page_config(
 with open('./database/about.txt', 'r') as f:
     about = f.read()
     st.markdown(about)
+     # Logo
+logo = Image.open('image/miflogo.PNG')
+st.image(logo, width=300)
+
+st.title('Map It Forward')
+st.markdown("participatory community mapping")

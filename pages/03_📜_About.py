8075d01a1e8a254a1d45162db9936549920917f9
Tina Zeng <121203814+txinjiez@users.noreply.github.com>
2023-09-09 17:45:42 -0400

    Update 03_📜_About.py

--- pages/03_📜_About.py
+++ pages/03_📜_About.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-
 with open('./database/about.txt', 'r') as f:
     about = f.read()
     st.markdown(about)
-     # Logo
+
+# Logo
 logo = Image.open('image/miflogo.PNG')
 st.image(logo, width=300)
 

0515a25b028c67875046142d757fd92b3ae2a434
Tina Zeng <121203814+txinjiez@users.noreply.github.com>
2023-09-09 17:46:49 -0400

    Update 03_📜_About.py

--- pages/03_📜_About.py
+++ pages/03_📜_About.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
+st.set_page_config(
+     page_title="ELRO Gameboard",
+     page_icon="🎲",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'About': "# Weekly games. Answer questions and compete against other grades"
+     }
+)
+
 with open('./database/about.txt', 'r') as f:
     about = f.read()
     st.markdown(about)
-
-# Logo
-logo = Image.open('image/miflogo.PNG')
-st.image(logo, width=300)
-
-st.title('Map It Forward')
-st.markdown("participatory community mapping")
